c8eb68287394bac277af6ef5d1295f0b732f8b84
divitbatra <divitbatra@hotmail.com>
2024-01-03 01:38:18 -0500

    Registered models with Admin site

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ['127.0.0.1',"localhost"]
+ALLOWED_HOSTS = ['127.0.0.1',"localhost","https://divitbatra-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai"]
 
 
 # Application definition
